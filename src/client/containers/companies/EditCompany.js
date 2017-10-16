b1abb4cb22aaa4a41637176cf0d1c607921514cb
Шестаков Павел <shestakov-pavel@bk.ru>
2017-10-17 01:00:57 +0300

    build error fix?

--- src/client/containers/companies/EditCompany.js
+++ src/client/containers/companies/EditCompany.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
-import EditCompanyComponent from '../../scenes/companies/EditCompany/index';
+import EditCompanyComponent from '../../scenes/Companies/EditCompany';
 import {
 	loadCompany
 } from "../../actionCreators/editCompany";
