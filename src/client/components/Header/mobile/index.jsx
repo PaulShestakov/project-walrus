145e38e8512d174955ad6eb32a9811a3c34de8e5
Шестаков Павел <shestakov-pavel@bk.ru>
2018-05-24 00:27:59 +0300

    Localization fix

--- src/client/components/Header/mobile/index.jsx
+++ src/client/components/Header/mobile/index.jsx
@@ -68,7 +68,7 @@ export default class MobileHeader extends React.PureComponent {
 										<ListItemIcon>
 											<PowerSettingsNew />
 										</ListItemIcon>
-										<ListItemText primary={Выйти} />
+										<ListItemText primary={'Выйти'} />
 									</ListItem>
 							}
 
